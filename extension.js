8d09b8b5b021c59f5b057ced5c0602b108409244
greatghoul <greatghoul@gmail.com>
2023-11-02 23:11:04 +0800

    match possible groups and examples and register langage selector

--- extension.js
+++ extension.js
@@ -1,5 +1,15 @@
 const vscode = require('vscode');
 
+const PATTERN_GROUP_OR_EXAMPLE = new RegExp([
+  '^\\s*(RSpec\.)?(',
+  'describe|context|it|test|should|',
+  'shared_examples_for|shared_examples|',
+  'shared_context|shared_context_for|',
+  'it_behaves_like|it_should_behave_like|',
+  'include_context|include_examples)',
+  '\\s+.*$'
+].join(''));
+
 class RubyTestSymbolProvider {
   provideDocumentSymbols(document, token) {
     return new Promise((resolve, reject) => {
@@ -7,10 +17,10 @@ class RubyTestSymbolProvider {
 
       for (var i = 0; i < document.lineCount; i++) {
         var line = document.lineAt(i);
-        if (line.text.startsWith("@")) {
+        if (PATTERN_GROUP_OR_EXAMPLE.test(line.text)) {
           symbols.push({
-            name: line.text.substr(1),
-            kind: vscode.SymbolKind.Field,
+            name: line.text.replace(/^\s+/gm, (match) => '\u200B '.repeat(match.length)),
+            kind: vscode.SymbolKind.Namespace,
             location: new vscode.Location(document.uri, line.range)
           });
         }
@@ -23,12 +33,9 @@ class RubyTestSymbolProvider {
 
 
 function activate(context) {
-
-	let provider = vscode.languages.registerDocumentSymbolProvider({ language: 'ruby' }, new RubyTestSymbolProvider());
+  let languageSelector = { language: 'ruby', pattern: '**/*_test.rb' };
+  let provider = vscode.languages.registerDocumentSymbolProvider(languageSelector, new RubyTestSymbolProvider());
 	context.subscriptions.push(provider);
-
-	vscode.window.showInformationMessage('Congratulations, your extension "ruby-test-symbols" is now active!');
-	console.log('Congratulations, your extension "ruby-test-symbols" is now active!');
 }
 
 function deactivate() {}
